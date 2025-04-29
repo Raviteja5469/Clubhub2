143570e706b49bc6dda2750cde988085c443eeee
Raviteja <ravitejaseguri@gmail.com>
2025-04-29 22:19:13 +0530

    Update CORS configuration to allow requests from the production domain

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ const app = express();
 
 // CORS setup
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://clubhub2.onrender.com/*',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
 }));
